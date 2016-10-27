360fc802b60c43dacbcc3fe45373bfc6a93333bd
Chris Bolin <bolin.chris@gmail.com>
2016-10-27 11:52:00 -0400

    add independent pages

--- js/index.js
+++ js/index.js
@@ -1,12 +1,6 @@
 var mobileMenu = document.getElementById("mobile-menu");
 var header = document.getElementById("header");
 
-var changePage = function(page){
-  mobileMenu.className = 'hide';
-  $('section[data-page='+page+']').removeClass('hide');
-  $('section[data-page!='+page+']').addClass('hide');  
-};
-
 document.onscroll = function(){
   if ($(document).scrollTop() > 1) {
     header.className = 'sticky';
@@ -24,7 +18,3 @@ $('#close-mobile-menu')
   .click(function(){
     mobileMenu.className = 'hide';
   });
-
-$('a.nav').click(function() {
-  changePage($(this).attr('data-link'));
-});
